54f56588d01fee4abfc34141c6ca13f50105a45b
Rodrigo Melo <rodrigo.melo199@gmail.com>
2015-12-05 18:00:03 -0200

    Initial commit

---
+++ app/src/main/java/br/com/gfinanceiro/MainActivity.java
@@ -0,0 +1,19 @@
+package br.com.gfinanceiro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kabom on 05/12/2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.adapter_despesas);
+
+    }
+}
